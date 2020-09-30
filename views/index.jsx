0dd82b59a076e3dce31c90bdbb9a86e555d6bc21
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-09-29 20:43:45 -0400

    added tooltips

--- views/index.jsx
+++ views/index.jsx
@@ -3,14 +3,23 @@ const Layout = require("./layout.jsx");
 
 class Index extends React.Component {
   render() {
+    // const {jokes} = this.props
     return (
       <Layout title="Issa Joke">
         <nav>
-          <span className="links"><a type="button" className="btn btn-outline-primary"href="/auth/signup">SignUp</a></span>
-          <span className="links"><a type="button" className="btn btn-outline-primary"href="/auth/login">Login</a></span>
+          <span className="links"><a type="button" className="btn btn-outline-primary"href="/auth/signup" data-toggle="tooltip" data-placement="left" title="Sign up to create, edit and delete your own jokes!">SignUp</a></span>
+          <span className="links"><a type="button" className="btn btn-outline-primary"href="/auth/login" data-toggle="tooltip" data-placement="right" title="Login to do the things">Login</a></span>
         </nav>
         <h1> {this.props.hello} </h1>
-        <h2 className="desc">Click on a joke title to reveal its contents</h2>
+        <h2 className="desc">Click a joke title to reveal its contents</h2>
+        {/* Jokes will go here */}
+        {/* {jokes.map((joke) => (
+          <div>
+            <h2>{joke.name}</h2>
+            <h3>{joke.buildUp}</h3>
+            <h3>{joke.punchLine}</h3>
+          </div> */}
+        {/* ))} */}
       </Layout>
     );
   }
